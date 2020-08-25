b8d8c899432c17c6eac965b9e8c0a19f37394cd1
gianninapg@gmail.com <Gi@090488>
2020-08-25 19:50:36 +1000

    create account

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     #add news url
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls'))
 ]
 #http://localhost:8000/users/login/
