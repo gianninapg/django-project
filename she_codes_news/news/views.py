0f33a8d60a4eb166cc6add61c4b1dde15c72542e
gianninapg@gmail.com <Gi@090488>
2020-08-25 20:19:35 +1000

    reload news

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -29,4 +29,8 @@ class AddStoryView(generic.CreateView):
     template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
